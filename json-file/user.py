f9cd07b91f10f6f7d27154d3ce84170a59e4b214
poswark <giovannyorjuel2@gmail.com>
2024-06-01 20:13:40 -0500

    creation branch trunk

--- json-file/user.py
+++ json-file/user.py
@@ -1,7 +1,7 @@
 import json
 import sys
 
-def add_data(usuario, edad, namespace):
+def agregar_usuario(usuario, edad, namespace, branch):
     archivo_json = 'template.json'
     
     try:
@@ -10,33 +10,55 @@ def add_data(usuario, edad, namespace):
     except FileNotFoundError:
         data = []
 
-    # Asegurarse de que data es una lista
-    if not isinstance(data, list):
-        data = []
-
-    # Agregar nuevo usuario con edad y namespace
-    data.append({
-        "name": usuario,
-        "edad": edad,
-        "namespace": namespace,
-        "host": {
-            "port": 1234,
-            "server": "boxung"
+    if branch == "dev":
+        nuevo_usuario = {
+            "name": usuario,
+            "edad": edad,
+            "namespace": namespace,
+            "host-dev": {
+                "port": 1234,
+                "server": "boxunging"
+            }
         }
-    })
+    elif branch == "uat":
+        nuevo_usuario = {
+            "name": usuario,
+            "edad": edad,
+            "namespace": namespace,
+            "host-uat": {
+                "port": 4321,
+                "server": "boxuguat"
+            }
+        }
+    elif branch == "prd":
+        nuevo_usuario = {
+            "name": usuario,
+            "edad": edad,
+            "namespace": namespace,
+            "host-prd": {
+                "port": 5678,
+                "server": "boxuprd"
+            }
+        }
+    else:
+        print(f"Rama desconocida: {branch}")
+        sys.exit(1)
+
+    data.append(nuevo_usuario)
     
     with open(archivo_json, 'w') as f:
         json.dump(data, f, indent=2)
     
-    print(f"Usuario {usuario} con edad {edad} y namespace {namespace} agregado correctamente.")
+    print(f"Usuario {usuario} agregado correctamente en la rama {branch}.")
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Uso: python actualizar_usuarios.py <nombre_usuario> <edad> <namespace>")
+    if len(sys.argv) != 5:
+        print("Uso: python actualizar_usuarios.py <nombre_usuario> <edad> <namespace> <rama>")
         sys.exit(1)
     
     nombre_usuario = sys.argv[1]
     edad = sys.argv[2]
     namespace = sys.argv[3]
+    branch = sys.argv[4]
     
-    add_data(nombre_usuario, edad, namespace)
+    agregar_usuario(nombre_usuario, edad, namespace, branch)
